c1faeb56e1adaae4df6a0384ecb2ee7433282fc0
tgp <wenliang.wang@partner.lingyue-digital.com>
2022-05-23 14:18:56 +0800

    add map

--- lib/src/model/device_part.dart
+++ lib/src/model/device_part.dart
@@ -4,12 +4,13 @@ class DevicePart {
   String? serial;
   String? phoneModel;
   String? board;
+  Map<String, String>? devicePartMap;
 
-  DevicePart(this.imei, this.ramInfo, this.serial, this.phoneModel, this.board);
-
+  DevicePart(this.imei, this.ramInfo, this.serial, this.phoneModel, this.board,
+      this.devicePartMap);
 
   @override
   String toString() {
-    return 'DevicePart{imei: $imei, ramInfo: $ramInfo, serial: $serial, phoneModel: $phoneModel, board: $board}';
+    return 'DevicePart{imei: $imei, ramInfo: $ramInfo, serial: $serial, phoneModel: $phoneModel, board: $board, devicePartMap: $devicePartMap}';
   }
 }
